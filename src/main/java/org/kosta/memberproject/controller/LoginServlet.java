cae5375b8a31d630c4caa85ad7ce060d759f846e
KimJoongHyeok <moon960427@gmail.com>
2021-10-23 01:38:04 +0900

    LoginServlet 구현

---
+++ src/main/java/org/kosta/memberproject/controller/LoginServlet.java
@@ -0,0 +1,43 @@
+package org.kosta.memberproject.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.memberproject.model.MemberDAO;
+import org.kosta.memberproject.model.MemberVO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String path = "";
+		try {
+			MemberVO vo = MemberDAO.getInstance().login(id, password);
+			
+			if(vo!=null) {
+				request.getSession().setAttribute("mvo", vo);
+				path = "test-login.jsp";	//index.jsp로 수정해야할 부분
+			}else {
+				path = "login-fail.jsp";
+			}
+			response.sendRedirect(path);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.sendRedirect("error.jsp");
+		}	
+	}
+}
