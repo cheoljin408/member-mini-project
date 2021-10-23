8db26e547497e4654772d683f88a220cf17e589d
KimJoongHyeok <moon960427@gmail.com>
2021-10-23 18:47:50 +0900

    LogoutServlet 구현 : 로그인상태에서 로그아웃 할 수 있는 서블릿

---
+++ src/main/java/org/kosta/memberproject/controller/LogoutServlet.java
@@ -0,0 +1,26 @@
+package org.kosta.memberproject.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		HttpSession session = request.getSession(false);
+		if(session!=null) {
+			session.invalidate();
+		}
+		response.sendRedirect("test-login.jsp");	//index.jsp 로 수정
+	}
+}
