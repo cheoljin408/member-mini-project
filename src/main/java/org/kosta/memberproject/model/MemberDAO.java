fd34d1b36a68e9233470d758a5c7cea89547e48b
KimJoongHyeok <moon960427@gmail.com>
2021-10-24 11:36:51 +0900

    MemberDAO update기능 추가 : 회원정보수정메서드(updateMember) 추가

--- src/main/java/org/kosta/memberproject/model/MemberDAO.java
+++ src/main/java/org/kosta/memberproject/model/MemberDAO.java
@@ -11,61 +11,62 @@ public class MemberDAO {
 	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 	private String username = "scott";
 	private String password = "tiger";
-	
+
 	private static MemberDAO instance = new MemberDAO();
-	
-	private MemberDAO() {}
-	
+
+	private MemberDAO() {
+	}
+
 	public static MemberDAO getInstance() {
 		return instance;
 	}
-	
+
 	public void closeAll(ResultSet rs, PreparedStatement pstmt, Connection con) throws SQLException {
-		if(rs != null)
+		if (rs != null)
 			rs.close();
 		closeAll(pstmt, con);
 	}
-	
+
 	public void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
-		if(pstmt != null)
+		if (pstmt != null)
 			pstmt.close();
-		if(con != null)
+		if (con != null)
 			con.close();
 	}
-	
+
 	private Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(url, username, password);
 	}
-	
+
 	public MemberVO findMemberById(String id) throws SQLException {
 		MemberVO vo = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			con = getConnection();
 			String sql = "select id, name, address from member where id=?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				vo = new MemberVO(rs.getString(1), null, rs.getString(2), rs.getString(3));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
 		}
-		
+
 		return vo;
 	}
-	
-	public MemberVO login(String id,String pass) throws SQLException {
+
+	public MemberVO login(String id, String pass) throws SQLException {
 		MemberVO vo = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			con = getConnection();
 			String sql = "select * from member where id = ? and password = ?";
@@ -73,10 +74,10 @@ public class MemberDAO {
 			pstmt.setString(1, id);
 			pstmt.setString(2, pass);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				vo = new MemberVO(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
+			if (rs.next()) {
+				vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
 			}
-		}finally {
+		} finally {
 			closeAll(rs, pstmt, con);
 		}
 		return vo;
@@ -84,26 +85,42 @@ public class MemberDAO {
 
 	public ArrayList<MemberVO> findMemberListByAddress(String address) throws SQLException {
 		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
-		
+
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			con = getConnection();
 			String sql = "select id, name from member where address=?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, address);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				list.add(new MemberVO(rs.getString(1), null, rs.getString(2), address));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
 		}
-		
+
 		return list;
 	}
-	
+
+	public void udpateMember(MemberVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = getConnection();
+			String sql = "update member set name = ? ,password = ?,address = ? where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getAddress());
+			pstmt.setString(4, vo.getId());
+			pstmt.executeUpdate();
+		} finally {
+			closeAll(pstmt, con);
+		}
+	}
 }
