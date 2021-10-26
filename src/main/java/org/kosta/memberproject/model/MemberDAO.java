7f9263afdc4f6352009cc31abd66ce93ba1e79b2
KimJoongHyeok <moon960427@gmail.com>
2021-10-26 21:24:36 +0900

    MemberDAO 리팩토링을 위한 메서드 전체 수정

--- src/main/java/org/kosta/memberproject/model/MemberDAO.java
+++ src/main/java/org/kosta/memberproject/model/MemberDAO.java
@@ -7,16 +7,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class MemberDAO {
-	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	private String username = "scott";
-	private String password = "tiger";
+import javax.sql.DataSource;
 
+public class MemberDAO {
+	
+	private DataSource dataSource;
 	private static MemberDAO instance = new MemberDAO();
 
 	private MemberDAO() {
+		this.dataSource = DataSourceManager.getInstance().getDataSource(); //DataSourceManager가 아직 없어서 생기는 오류
 	}
-
+	
 	public static MemberDAO getInstance() {
 		return instance;
 	}
@@ -34,10 +35,6 @@ public class MemberDAO {
 			con.close();
 	}
 
-	private Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(url, username, password);
-	}
-
 	public MemberVO findMemberById(String id) throws SQLException {
 		MemberVO vo = null;
 		Connection con = null;
@@ -45,14 +42,16 @@ public class MemberDAO {
 		ResultSet rs = null;
 
 		try {
-			con = getConnection();
-			String sql = "select id, name, address from member where id=?";
-			pstmt = con.prepareStatement(sql);
+			con = dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("select id,name,address,to_char(birth,'yyyy-mm-dd')");
+			sql.append(",to_char(regdate,'yyyy-mm-dd HH24:MI:SS') from mvc_member where id = ?"); //id로 회원검색시 pass빼고 정보 모두 출력하게 했습니다~
+			pstmt = con.prepareStatement(sql.toString());
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 
 			if (rs.next()) {
-				vo = new MemberVO(rs.getString(1), null, rs.getString(2), rs.getString(3));
+				vo = new MemberVO(rs.getString(1), null, rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
@@ -68,14 +67,14 @@ public class MemberDAO {
 		ResultSet rs = null;
 
 		try {
-			con = getConnection();
-			String sql = "select * from member where id = ? and password = ?";
+			con = dataSource.getConnection();
+			String sql = "select * from mvc_member where id = ? and password = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pass);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+				vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),rs.getString(5),rs.getString(6));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
@@ -91,14 +90,14 @@ public class MemberDAO {
 		ResultSet rs = null;
 
 		try {
-			con = getConnection();
-			String sql = "select id, name from member where address=?";
+			con = dataSource.getConnection();
+			String sql = "select id, name ,to_char(birth,'yyyy-mm-dd') from mvc_member where address=?";	//주소로 회원검색시 id,name,birth 까지만 나오게 했습니다~
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, address);
 			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
-				list.add(new MemberVO(rs.getString(1), null, rs.getString(2), address));
+				list.add(new MemberVO(rs.getString(1), null, rs.getString(2), address,rs.getString(3),null));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
@@ -111,53 +110,56 @@ public class MemberDAO {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		try {
-			con = getConnection();
-			String sql = "update member set name = ? ,password = ?,address = ? where id = ?";
+			con = dataSource.getConnection();
+			String sql = "update mvc_member set name = ? ,password = ?,address = ?,birth = to_date(?,'yyyy-mm-dd') where id = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getPassword());
 			pstmt.setString(3, vo.getAddress());
-			pstmt.setString(4, vo.getId());
+			pstmt.setString(4, vo.getBirth());
+			pstmt.setString(5, vo.getId());
 			pstmt.executeUpdate();
 		} finally {
 			closeAll(pstmt, con);
 		}
 	}
-	
-	public void register(MemberVO vo) throws SQLException{
+
+	public void register(MemberVO vo) throws SQLException {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		try {
-			con = getConnection();
-			String sql = "insert into member(id,password,name,address) values(?,?,?,?)";
-			pstmt = con.prepareStatement(sql);
+			con = dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("insert into mvc_member(id,password,name,address,birth,regdate)");
+			sql.append(" values(?,?,?,?,do_date(?,'yyyy-mm-dd'),sysdate)");
+			pstmt = con.prepareStatement(sql.toString());
 			pstmt.setString(1, vo.getId());
 			pstmt.setString(2, vo.getPassword());
 			pstmt.setString(3, vo.getName());
 			pstmt.setString(4, vo.getAddress());
+			pstmt.setString(5, vo.getBirth());
 			pstmt.executeUpdate();
-		}finally {
+		} finally {
 			closeAll(pstmt, con);
 		}
 	}
-	
-	public boolean idcheck(String id) throws SQLException{
+
+	public boolean idcheck(String id) throws SQLException {
 		boolean result = false;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			con = DriverManager.getConnection(url, username, password);
-			String sql = "select count(*) from member where id = ?";
+			con = dataSource.getConnection();
+			String sql = "select count(*) from mvc_member where id = ?";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1,id);
+			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-			if(rs.next() && rs.getInt(1)==1) 
+			if (rs.next() && rs.getInt(1) == 1)
 				result = true;
-		}finally {
+		} finally {
 			closeAll(rs, pstmt, con);
 		}
 		return result;
 	}
-	
 }
