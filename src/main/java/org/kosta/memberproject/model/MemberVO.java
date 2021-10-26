6044f0ec25721f9883a72a78727d429b4a9b3528
KimJoongHyeok <moon960427@gmail.com>
2021-10-26 21:25:27 +0900

    MemberVO 인스턴스추가(birth,regdate)

--- src/main/java/org/kosta/memberproject/model/MemberVO.java
+++ src/main/java/org/kosta/memberproject/model/MemberVO.java
@@ -1,21 +1,26 @@
 package org.kosta.memberproject.model;
 
+
 public class MemberVO {
 	private String id;
 	private String password;
 	private String name;
 	private String address;
+	private String birth;
+	private String regdate;
 
 	public MemberVO() {
 		super();
 	}
-	
-	public MemberVO(String id, String password, String name, String address) {
+
+	public MemberVO(String id, String password, String name, String address, String birth, String regdate) {
 		super();
 		this.id = id;
 		this.password = password;
 		this.name = name;
 		this.address = address;
+		this.birth = birth;
+		this.regdate = regdate;
 	}
 
 	public String getId() {
@@ -50,9 +55,26 @@ public class MemberVO {
 		this.address = address;
 	}
 
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
 	@Override
 	public String toString() {
-		return "MemberVO [id=" + id + ", password=" + password + ", name=" + name + ", address=" + address + "]";
+		return "MemberVO [id=" + id + ", password=" + password + ", name=" + name + ", address=" + address + ", birth="
+				+ birth + ", regdate=" + regdate + "]";
 	}
-	
+
 }
