9a4884b1c939835a505f22229886f1fac70b4d34
Liiims <sehee167@naver.com>
2021-10-27 20:04:08 +0900

    FrontControllerServlet 추가

---
+++ src/main/java/org/kosta/memberproject/controller/FrontControllerServlet.java
@@ -0,0 +1,49 @@
+package org.kosta.memberproject.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontControllerServlet
+ */
+@WebServlet("*.do")
+public class FrontControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doDispatch(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String uri = request.getRequestURI();
+			String contextPath = request.getContextPath();
+			String command = uri.substring(contextPath.length()+1, uri.length()-3);
+																						
+			Controller controller = HandlerMapping.getInstance().create(command);
+			String path = controller.execute(request, response);
+			if (path.startsWith("redirect:")) {
+				response.sendRedirect(path.substring(9));
+			} else {
+				request.getRequestDispatcher(path).forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doDispatch(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		this.doDispatch(request, response);
+	}
+
+}
